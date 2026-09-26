660de467a4af8bc01a9b340163ea1ceaf91cf38b
cchanlan <2606138772@qq.com>
2026-09-27 03:24:23 +0800

    feat(适配): 兼容无 miao-plugin 环境（如 JiuLi 极简部署）
    - index.js 改为动态检测 miao-plugin 是否存在
    - 存在时正常启用全部功能，不存在时优雅降级
    - 8 个依赖 miao-plugin 的 apps 改为条件 import
    - 避免插件加载阶段直接崩溃

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
 // 插件入口：Yunzai 有 index.js 时只加载本文件导出，不会自动扫 apps/*
-import './utils/ckAutoRefresh.js' // ck 失效全局自愈：加载即对 genshin checkCode 打补丁
+import './utils/ckAutoRefresh.js'
 import { TL } from './apps/TL.js'
 import { Abyss } from './apps/Abyss.js'
-import { role_combat } from './apps/role_combat.js'
-import { miniRoleCombat } from './apps/miniRoleCombat.js'
-import { gsAllAbyss } from './apps/gsAllAbyss.js'
-import { abyssTeam } from './apps/abyssTeam.js'
-import { hardTeam } from './apps/hardTeam.js'
-import { holdRate } from './apps/holdRate.js'
-import { teamDamage } from './apps/teamDamage.js'
 import { TmpCleaner } from './apps/tmpCleaner.js'
 import { nanokaAbyss } from './apps/nanokaAbyss.js'
 import { help } from './apps/help.js'
@@ -16,29 +9,35 @@ import { resinPush } from './apps/resinPush.js'
 import { autoSign } from './apps/autoSign.js'
 import { autoBbsCoin } from './apps/autoBbsCoin.js'
 import { TLDelCkHook } from './apps/delCkHook.js'
-import { srGachaLog } from './apps/srGachaLog.js'
-import { captchaNotice } from './apps/captchaNotice.js'
 import { solverDeploy } from './apps/solverDeploy.js'
 
-export {
-  TL,
-  Abyss,
-  role_combat,
-  miniRoleCombat,
-  gsAllAbyss,
-  abyssTeam,
-  hardTeam,
-  holdRate,
-  teamDamage,
-  TmpCleaner,
-  nanokaAbyss,
-  help,
-  resinPush,
-  autoSign,
-  autoBbsCoin,
-  TLDelCkHook,
-  srGachaLog,
-  captchaNotice,
-  solverDeploy,
+const hasMiaoPlugin = (() => {
+  try {
+    const botRoot = new URL('../../..', import.meta.url).pathname
+    return fs.existsSync(botRoot + 'plugins/miao-plugin/models/index.js')
+  } catch { return false }
+})()
+
+let teamDamage, role_combat, miniRoleCombat, gsAllAbyss, abyssTeam, hardTeam, holdRate, srGachaLog
+
+if (hasMiaoPlugin) {
+  const m1 = await import('./apps/teamDamage.js'); teamDamage = m1.teamDamage
+  const m2 = await import('./apps/role_combat.js'); role_combat = m2.role_combat
+  const m3 = await import('./apps/miniRoleCombat.js'); miniRoleCombat = m3.miniRoleCombat
+  const m4 = await import('./apps/gsAllAbyss.js'); gsAllAbyss = m4.gsAllAbyss
+  const m5 = await import('./apps/abyssTeam.js'); abyssTeam = m5.abyssTeam
+  const m6 = await import('./apps/hardTeam.js'); hardTeam = m6.hardTeam
+  const m7 = await import('./apps/holdRate.js'); holdRate = m7.holdRate
+  const m8 = await import('./apps/srGachaLog.js'); srGachaLog = m8.srGachaLog
+  if (globalThis.logger) logger.info('[xhh-TL] miao-plugin detected, all features enabled')
+} else {
+  const placeholder = class { constructor() { this.rule = [] } }
+  teamDamage = role_combat = miniRoleCombat = gsAllAbyss = abyssTeam = hardTeam = holdRate = srGachaLog = placeholder
+  if (globalThis.logger) logger.warn('[xhh-TL] miao-plugin not found, 8 features disabled')
 }
 
+export {
+  TL, Abyss, teamDamage, role_combat, miniRoleCombat, gsAllAbyss, abyssTeam, hardTeam, holdRate,
+  TmpCleaner, nanokaAbyss, help, resinPush, autoSign, autoBbsCoin,
+  TLDelCkHook, srGachaLog, solverDeploy,
+}
